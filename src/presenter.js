ffaa0ff3dd526a8e48204bfd76cafdc174a137f6
Nicolette <nicolette.chalup@ucb.edu.bo>
2024-03-06 16:05:37 -0400

    Ingresa el precio por item y muestra en pantalla lo ingresado

--- src/presenter.js
+++ src/presenter.js
@@ -1,14 +1,18 @@
 import totalizador from "./totalizador";
 
 const cantidad = document.querySelector("#cantidad");
+const precio = document.querySelector("#precio");
 const form = document.querySelector("#totalizador-form");
 const div = document.querySelector("#resultado1-div");
+const div2 = document.querySelector("#resultado2-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const Cantidad = Number.parseInt(cantidad.value);
+  const Precio = Number.parseInt(precio.value);
 
 
-  div.innerHTML = "<p>" + totalizador(Cantidad) + "</p>";
+  div.innerHTML = "<p> Cantidad: " + totalizador(Cantidad) + "</p>";
+  div2.innerHTML = "<p> Precio: " + totalizador(Precio) + "</p>";
 });
--- src/totalizador.spec.js
+++ src/totalizador.spec.js
@@ -4,4 +4,7 @@ describe("Totalizador", () => {
   it("deberia mostrar en pantalla la cantidad de items ingresado", () => {
     expect(totalizador(3)).toEqual(3);
   });
+  it("deberia mostrar en pantalla la Precio por item ingresado", () => {
+    expect(totalizador(4)).toEqual(4);
+  });
 });
